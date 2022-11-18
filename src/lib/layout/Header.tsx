e08bebc6cfd6540bf22b7be2d4d67f08e3ce9d80
norris <33255034+Gsirron@users.noreply.github.com>
2022-11-18 11:20:04 +1100

    chore: updated themes

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -6,6 +6,7 @@ import {
   Link,
   IconButton,
   useDisclosure,
+  Container,
 } from "@chakra-ui/react";
 
 const NavBarLinks = [
@@ -17,35 +18,42 @@ const NavBarLinks = [
 const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Flex
+    <Box
       h="5em"
       width="full"
-      justifyContent="space-between"
-      // bgGradient="linear(to-r, blue.800, gray.800,blue.900)"
       bgGradient="radial( blue.800,gray.700)"
       py="1em"
       px={["1em", "2em"]}
+      justifyContent="center"
     >
-      <Box justifyContent="center">
-        <IconButton
-          variant="outline"
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon boxSize="1.5em" />}
-          aria-label="Open Menu"
-          display={{ md: "none" }}
-          onClick={isOpen ? onClose : onOpen}
-        />
-      </Box>
+      <Flex
+        py="1em"
+        as={Container}
+        justifyContent="space-between"
+        // bgGradient="linear(to-r, blue.800, gray.800,blue.900)"
+        maxW="7xl"
+      >
+        <Box justifyContent="center">
+          <IconButton
+            variant="outline"
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon boxSize="1.5em" />}
+            aria-label="Open Menu"
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+        </Box>
 
-      <IconButton variant="link" aria-label="home" />
-      <HStack spacing="2em" display={{ base: "none", md: "flex" }}>
-        {NavBarLinks.map((linkItem) => (
-          <Link key={linkItem.name} href={linkItem.link}>
-            {linkItem.name}
-          </Link>
-        ))}
-      </HStack>
-      {/* <Box><ThemeToggle /></Box> */}
-    </Flex>
+        <IconButton variant="link" aria-label="home" icon={<HamburgerIcon />} />
+        <HStack spacing="2em" display={{ base: "none", md: "flex" }}>
+          {NavBarLinks.map((linkItem) => (
+            <Link key={linkItem.name} href={linkItem.link}>
+              {linkItem.name}
+            </Link>
+          ))}
+        </HStack>
+        {/* <Box><ThemeToggle /></Box> */}
+      </Flex>
+    </Box>
   );
 };
 
