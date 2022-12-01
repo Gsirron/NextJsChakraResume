a766b70c3632576b829a561c5d7aa214d531bc84
norris <33255034+Gsirron@users.noreply.github.com>
2022-12-01 11:34:08 +1100

    chore: updates seo config a bit

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "nextarter-chakra",
-  titleTemplate: "%s | nextarter-chakra",
-  defaultTitle: "nextarter-chakra",
-  description: "Next.js + chakra-ui + TypeScript template",
+  title: "Norris Luong",
+  titleTemplate: "%s | Norris Luong Personal Website",
+  defaultTitle: "Norris Luong Personal Website",
+  description: "Norris Luong's Personal Website",
   canonical: "https://nextarter-chakra.sznm.dev",
   openGraph: {
     url: "https://nextarter-chakra.sznm.dev",
-    title: "nextarter-chakra",
-    description: "Next.js + chakra-ui + TypeScript template",
+    title: "Norris Luong Personal Website",
+    description: "Norris Luong's Personal Website",
     images: [
       {
         url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
         alt: "nextarter-chakra.sznm.dev og-image",
       },
     ],
-    site_name: "nextarter-chakra",
+    site_name: "Norris Luong Personal Website",
   },
   twitter: {
     handle: "@sozonome",
--- src/lib/pages/about/index.tsx
+++ src/lib/pages/about/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Center, Flex, Heading } from "@chakra-ui/react";
 import React from "react";
 
 type AboutProps = {
