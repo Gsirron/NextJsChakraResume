0d82910ebd2b2de46e442925b7da15d6d1253177
norris <33255034+Gsirron@users.noreply.github.com>
2022-12-02 20:20:51 +1100

    chore updated eslint configs and layout

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Flex,
   Stack,
@@ -13,6 +12,7 @@ import {
   Center,
 } from "@chakra-ui/react";
 import { NextSeo } from "next-seo";
+import React from "react";
 import { FaDownload } from "react-icons/fa";
 
 import ResumeData from "@/data/ResumeData";
@@ -27,9 +27,9 @@ const Home = () => {
   const textColor = "blue.600";
   return (
     <Stack direction={["column", "row"]} spacing={["1em", "2em"]}>
-      <NextSeo title="Home" />
+      <NextSeo title={"Home"} />
       <Stack
-        align="center"
+        align={"center"}
         spacing={["1em", "2em", "3em"]}
         py={["1em", "2em", "3em"]}
         direction={{ base: "column", md: "row" }}
@@ -42,8 +42,8 @@ const Home = () => {
           >
             <Text color={textColor}>{`Hi I'm`}</Text>
             <Text
-              as="span"
-              position="relative"
+              as={"span"}
+              position={"relative"}
               _after={{
                 content: "''",
                 width: "full",
@@ -62,28 +62,28 @@ const Home = () => {
             <Text color={textColor}>{`I'm a \n
               ${Occupation}`}</Text>
           </Heading>
-          <Text color="gray.300" fontSize={["md", "lg", "xl"]}>
+          <Text color={"gray.300"} fontSize={["md", "lg", "xl"]}>
             {Bio}
           </Text>
 
           <Stack
             spacing={{ base: "1em", sm: "1.5em" }}
             direction={{ base: "column", sm: "row" }}
-            justifyContent="center"
+            justifyContent={"center"}
             // justifyItems={"center"}
           >
             <SocialMedia BtnSize={"2em"} />
             <Button
-              as="a"
+              as={"a"}
               href={ResumeLink}
-              target="_blank"
+              target={"_blank"}
               // maxW={"20rem"}
-              rounded="full"
-              size="lg"
-              fontWeight="normal"
-              px="3em"
-              colorScheme="blue"
-              bg="blue.600"
+              rounded={"full"}
+              size={"lg"}
+              fontWeight={"normal"}
+              px={"3em"}
+              colorScheme={"blue"}
+              bg={"blue.600"}
               _hover={{ bg: "blue.500" }}
               leftIcon={<FaDownload />}
             >
@@ -93,10 +93,10 @@ const Home = () => {
         </Stack>
         <Flex
           flex={1}
-          justify="center"
-          align="center"
-          position="relative"
-          w="full"
+          justify={"center"}
+          align={"center"}
+          position={"relative"}
+          w={"full"}
         >
           {/* <Blob
               w={"150%"}
@@ -108,32 +108,34 @@ const Home = () => {
               color={useColorModeValue("red.50", "red.400")}
             /> */}
           <Box
-            position="relative"
-            height="300px"
-            rounded="2xl"
-            boxShadow="2xl"
-            width="full"
-            overflow="hidden"
+            position={"relative"}
+            height={"300px"}
+            rounded={"2xl"}
+            boxShadow={"2xl"}
+            width={"full"}
+            overflow={"hidden"}
           >
             <IconButton
-              aria-label="Play Button"
-              variant="ghost"
+              aria-label={"Play Button"}
+              variant={"ghost"}
               _hover={{ bg: "transparent" }}
               // icon={<PlayIcon w={12} h={12} />}
-              size="lg"
-              color="white"
-              position="absolute"
-              left="50%"
-              top="50%"
-              transform="translateX(-50%) translateY(-50%)"
+              size={"lg"}
+              color={"white"}
+              position={"absolute"}
+              left={"50%"}
+              top={"50%"}
+              transform={"translateX(-50%) translateY(-50%)"}
             />
             <Image
-              alt="Hero Image"
-              fit="cover"
-              align="center"
-              w="100%"
-              h="100%"
-              src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
+              alt={"Hero Image"}
+              fit={"cover"}
+              align={"center"}
+              w={"100%"}
+              h={"100%"}
+              src={
+                "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
+              }
             />
           </Box>
         </Flex>
