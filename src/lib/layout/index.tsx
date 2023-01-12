8a7986d9b7f5bed59eabe64b01c0a2d75fe7088e
norris <33255034+Gsirron@users.noreply.github.com>
2023-01-12 11:34:07 +1100

    chore: updated yarn and packages

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -5,20 +5,17 @@ import ConfettiBg from "./backgrounds/ConfettiBg";
 import Footer from "./Footer";
 import Header from "./header";
 
-interface SectionProps {
+interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const { CONFETTI_LIGHT, CONFETTI_DARK } = ConfettiBg;
-
   return (
     <Box
       //
       bg={"gray.100"}
       margin={"0 auto"}
       transition={"0.5s ease-out"}
-      bgAttachment={"fixed"}
     >
       <Header />
 
